8a3cfa80748aac4102dffa0ed793544ad7a2d098
Mohamed Natour <developer1pro@gmail.com>
2022-04-20 14:52:45 +0300

    work in feature 1.0, delete and insert is D O N E !!

--- src/BacktrackingArray.java
+++ src/BacktrackingArray.java
@@ -1,9 +1,9 @@
 
 
 public class BacktrackingArray implements Array<Integer>, Backtrack {
-    private Stack stack;
-    private int Volume = 0;
-    private int[] arr;
+    public Stack stack;
+    public int Volume = 0;
+    public int[] arr;
     // TODO: implement your code here
 
     // Do not change the constructor's signature
@@ -45,7 +45,19 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     @Override
     public void delete(Integer index) {
         // TODO: implement your code here
+        // delete with theta(n)
 
+        // validate when arr is empty??
+
+        int[] meta = {1, index.intValue(), this.arr[index.intValue()]};
+        this.stack.push(meta);
+
+        for(int i = index.intValue(); i < this.arr.length - 1; i++ ){
+            this.arr[i] = this.arr[i + 1];
+
+        }
+
+        this.Volume = this.Volume - 1;
 
     }
 
@@ -76,7 +88,19 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     @Override
     public void backtrack() {
         // TODO: implement your code here
+        // validate when stack is empty??
 
+        int [] stackPop = (int[]) this.stack.pop();
+        if(stackPop[0] == 0){
+            this.Volume = this.Volume - 1;
+        } else if (stackPop[0] == 1) {
+            for(int i = this.Volume ; i > stackPop[1]; i--){
+                this.arr[i] = this.arr[i - 1];
+            }
+
+            this.arr[stackPop[1]] = stackPop[2];
+            this.Volume = this.Volume + 1;
+        }
     }
 
     @Override
--- src/Test.java
+++ src/Test.java
@@ -7,27 +7,29 @@ public class Test {
         int x = 4;
         int forward = 3;
         int back = 2;
+        Stack stack = new Stack();
+
+        BacktrackingArray unsarray = new BacktrackingArray(stack, 5);
+        unsarray.insert(new Integer(1));
+        unsarray.insert(new Integer(2));
+        unsarray.insert(new Integer(7));
+        unsarray.insert(new Integer(3));
+        System.out.println(Arrays.toString(unsarray.arr));
+
+        unsarray.delete(new Integer(1));
+        System.out.println(Arrays.toString(unsarray.arr) + "first  delete");
+        unsarray.delete(new Integer(2));
+        System.out.println(Arrays.toString(unsarray.arr)  + "second  delete");
+
+        unsarray.backtrack();
+        System.out.println(Arrays.toString(unsarray.arr) + "first  backtrack");
+        unsarray.backtrack();
 
 
-//        Warmup test = new Warmup();
-        Stack test2 = new Stack();
-        int [] pop = new int[2];
-        test2.push(pop);
-        int [] res = {1,2,3};
+        System.out.println(Arrays.toString(unsarray.arr) + "second  backtrack");
+        System.out.println(unsarray.Volume);
 
-        System.out.print(Arrays.toString(res));
 
-        //test backtracking array
-
-//        BacktrackingArray test = new BacktrackingArray(test2, 3);
-//        test.insert(new Integer(1));
-//        test.insert(new Integer(2));
-//        test.insert(new Integer(3));
-//
-//
-//        int [] k = new int[3];
-//
-//
     }
 
 }
