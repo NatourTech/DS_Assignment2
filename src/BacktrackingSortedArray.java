db6fcf0053710db340538c84e0e12765fe135dd6
Mohamed Natour <developer1pro@gmail.com>
2022-04-21 07:04:43 +0300

    work in feature 1.0, update for Sorted Array ,delete, insert,Search,print, get is D O N E !!

--- src/BacktrackingSortedArray.java
+++ src/BacktrackingSortedArray.java
@@ -5,6 +5,8 @@ public class BacktrackingSortedArray implements Array<Integer>, Backtrack {
     public int[] arr; // This field is public for grading purposes. By coding conventions and best practice it should be private.
     // TODO: implement your code here
 
+    private int volume = 0;
+
     // Do not change the constructor's signature
     public BacktrackingSortedArray(Stack stack, int size) {
         this.stack = stack;
@@ -14,23 +16,65 @@ public class BacktrackingSortedArray implements Array<Integer>, Backtrack {
     @Override
     public Integer get(int index){
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+
+        // TODO:validate if index is legal??
+        int res = arr[index];
+    	return res; // temporal return command to prevent compilation error
     }
 
     @Override
     public Integer search(int k) {
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        int low = 0, high = volume - 1;
+
+        while(low <= high){
+
+            int middle = (low + high) / 2;
+
+            if(arr[middle] == k){
+                return middle;
+            }else if ( k < arr[middle]){
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+
+    	return -1; // temporal return command to prevent compilation error
     }
 
     @Override
     public void insert(Integer x) {
         // TODO: implement your code here
+        if(volume >= arr.length){
+            throw new RuntimeException("The Array is full!!");
+        }
+
+        int i = volume;
+        while( i > 0 && arr[i - 1]  > x){
+            arr[i] = arr[i - 1];
+            i = i - 1;
+        }
+
+        int[] meta = {0,i,x};
+        arr[i] = x;
+        stack.push(meta);
+        volume = volume + 1;
+
     }
 
     @Override
     public void delete(Integer index) {
         // TODO: implement your code here
+        if(volume == 0 || index > volume - 1 || index < 0){
+            throw new RuntimeException("index not valid, or array is empty pleas check!!");
+        }
+        int[] meta = {1,index, arr[index]};
+        for(int i = index; i < volume; i ++ ){
+            arr[i] = arr[i+1];
+        }
+        stack.push(meta);
+        volume = volume - 1;
     }
 
     @Override
@@ -72,6 +116,10 @@ public class BacktrackingSortedArray implements Array<Integer>, Backtrack {
     @Override
     public void print() {
         // TODO: implement your code here
+
+        for(int i = 0; i < volume; i++) {
+            System.out.print(arr[i]+" ");
+        }
     }
     
 }
--- src/Test.java
+++ src/Test.java
@@ -9,25 +9,41 @@ public class Test {
         int back = 2;
         Stack stack = new Stack();
 
-        BacktrackingArray unsarray = new BacktrackingArray(stack, 12);
-        unsarray.insert(new Integer(1));
-        unsarray.insert(new Integer(7));
-        unsarray.insert(new Integer(3));
-        unsarray.insert(new Integer(-2));
-        unsarray.insert(new Integer(-4));
-        unsarray.insert(new Integer(-11));
-        unsarray.insert(new Integer(-1));
-        unsarray.insert(new Integer(2));
-        unsarray.insert(new Integer(5));
-        System.out.println(Arrays.toString(unsarray.arr));
+
+//        BacktrackingArray unsarray = new BacktrackingArray(stack, 12);
+//        unsarray.insert(new Integer(1));
+//        unsarray.insert(new Integer(7));
+//        unsarray.insert(new Integer(3));
+//        unsarray.insert(new Integer(-2));
+//        unsarray.insert(new Integer(-4));
+//        unsarray.insert(new Integer(-11));
+//        unsarray.insert(new Integer(-1));
+//        unsarray.insert(new Integer(2));
+//        unsarray.insert(new Integer(5));
+//        System.out.println(Arrays.toString(unsarray.arr));
+//
+//
+//        System.out.println(unsarray.successor(0));
+//        System.out.println(unsarray.predecessor(0));
+//        unsarray.print();
 
 
-        System.out.println(unsarray.successor(0));
-        System.out.println(unsarray.predecessor(0));
-        unsarray.print();
+        Stack stack2 = new Stack();
+        // sorted array obj
+        BacktrackingSortedArray soArray = new BacktrackingSortedArray(stack2, 10);
+        soArray.insert(6);
+        soArray.insert(7);
+        soArray.insert(2);
+        soArray.insert(3);
+        soArray.insert(-4);
+        soArray.insert(-1);
+        soArray.insert(-19);
+        soArray.print();
 
+        soArray.delete(0);
 
-
+        System.out.println("");
+        soArray.print();
 
 
     }
